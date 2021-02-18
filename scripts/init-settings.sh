4445066b04a26d9f3df6c1ccd3d5575b82809e61
wyap <mr.xdpool@gmail.com>
2021-02-19 01:00:13 +0800

    “测试”

---
+++ scripts/init-settings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#=================================================
+# File name: init-settings.sh
+# Description: This script will be executed during the first boot
+# Author: SuLingGG
+# Blog: https://mlapp.cn
+#=================================================
+
+# Set default theme to luci-theme-argon
+uci set luci.main.mediaurlbase='/luci-static/argon'
+
+# Disable opkg signature check
+sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
+
+exit 0
