4d312d20ba03802e12795d1358d74cb10c58ca4c
wyap-echo <58325947+wyap-echo@users.noreply.github.com>
2021-03-04 00:56:34 +0800

    Update project-lede-openwrt.sh

--- scripts/project-lede-openwrt.sh
+++ scripts/project-lede-openwrt.sh
@@ -30,6 +30,9 @@ popd
 #sed -i 's/ImmortalWrt/OpenWrt/g' config_generate
 #popd
 
+# Fix SDK
+sed -i '/$(SDK_BUILD_DIR)\/$(STAGING_SUBDIR_HOST)\/usr\/bin/d;/LICENSE/d' target/sdk/Makefile
+
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
